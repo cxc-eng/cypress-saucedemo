bb1edfadf180b0f952585b066afbcce294a4fd3e
Karol Hajdyła <hear.me.right.now@gmail.com>
2020-12-14 14:48:58 +0100

    Feature: Logging with invalid credentials

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -19,4 +19,30 @@ context('Feature: Logging', () => {
           cy.logout();
       })
     });
+
+    it('doesn\'t log in with invalid credentials', () => {
+
+        const invalidCredentialsAndErrorMessages = [
+            {login: 'locked_out_user', password: 'secret_sauce'},
+            {login: '{leftarrow}', password: 'secret_sauce'},
+            {login: 'performance_glitch_user', password: '{leftarrow}'},
+            {login: 'fakeusername', password: 'fakepassword'}
+        ];
+
+        const errorMessages = [
+            'Sorry, this user has been locked out',
+            'Epic sadface: Username is required',
+            'Epic sadface: Password is required',
+            'Epic sadface: Username and password do not match any user in this service',
+        ]
+  
+        invalidCredentialsAndErrorMessages.forEach((user, index) => {
+            cy.login(user);
+  
+            cy.get('h3 button.error-button').should('be.visible');
+            cy.get('h3[data-test="error"]').should('contain.text', errorMessages[index]);
+
+            cy.reload();
+        })
+      })
   })
